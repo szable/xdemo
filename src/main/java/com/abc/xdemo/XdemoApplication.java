fc8e90860f4c00dcebe7a06ca77771513625ff6d
szable <sz_jxf@163.com>
2019-08-03 09:01:32 +0800

    init repo

---
+++ src/main/java/com/abc/xdemo/XdemoApplication.java
@@ -0,0 +1,13 @@
+package com.abc.xdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdemoApplication.class, args);
+	}
+
+}
