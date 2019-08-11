2f7ae90c48d731c1101b88eb82e0220388111b2e
szable <sz_jxf@163.com>
2019-08-11 17:45:22 +0800

    add publish

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## name project
 
-
+## flyway
--- src/main/java/com/abc/xdemo/controller/PlublishController.java
+++ src/main/java/com/abc/xdemo/controller/PlublishController.java
@@ -1,4 +1,13 @@
 package com.abc.xdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PlublishController {
+
+    @RequestMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
 }
